c654b04cd91aabf0a563cf327e7e1495d1a8abd6
Jesus <117941643+JesusV545@users.noreply.github.com>
2023-02-26 22:54:40 -0600

    added some comments and added a validation function for text length

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 //installing inquirer and fs
 const inquirer = require('inquirer');
-const { readFile, writeFile } = require('fs/promises')
+const { writeFile } = require('fs/promises')
 
 //the class needed to construct the logo with user info
 const { LogoGen, Square, Triangle, Circle } = require('./lib/shapes');
 
 //start the question prompts
 inquirer.prompt([
+    //need to ask the basic questions to make logo
     {
         type: 'input',
         name: 'text',
         message: 'What text do you want on your logo? (The limit is 3 characters!)',
-        //validate: validate
+        validate: validate
     },
 
     {
@@ -34,9 +35,10 @@ inquirer.prompt([
         message: 'What color do you want your shape to be?',
         choices: ['red', 'blue', 'green']
     }
-
+    //use the data
 ]).then((data) => {
     let shape;
+    //need to make the shape the user chose using classes
     switch (data.shape) {
         case 'circle':
             shape = new Circle(data.shape_color);
@@ -50,23 +52,17 @@ inquirer.prompt([
           default:
             throw new Error('Invalid shape type');
         }
+        //need to save the shape data to use later
         const logoData = {
             text: data.text,
             text_color: data.text_color,
             shape: shape
         };
+        //need to use the final data to create the logo with LogoGen class
         const logo = new LogoGen(logoData).render();
+        //need to make a new svg file 
         return writeFile('./examples/logo.svg', logo);
     
-//     const newLogo = new LogoGen(
-//         data.text,
-//         data.text_color,
-//         data.shape,
-//         data.shape_color
-// );
-//     //use render() to create the logo
-//     const logo = newLogo.render();
-
 
 }).then(() => {
     //let the user know that their logo was created successfully
@@ -79,3 +75,10 @@ inquirer.prompt([
 
 });
 
+function validate(text){
+    if (text.length > 3){
+        throw new Error('Cannot be more than 3 characters!');
+    } else {
+        return true;
+    }
+}
